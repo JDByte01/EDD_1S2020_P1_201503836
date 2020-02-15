2d0b495af6bd8ccbe34bcaff9c65e26d9ba4e107
KByteGt <josedan1996@gmail.com>
2020-02-15 11:51:54 -0600

    Dia 3 la muerte

---
+++ ListaSimple/ListaSimple.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+import "NodoPalabra.cpp"
+
+class ListaSimple
+{
+private:
+    /* data */
+public:
+    ListaSimple(/* args */);
+    ~ListaSimple();
+};
+
+/* ============================= */
+ListaSimple::ListaSimple(/* args */)
+{
+}
+
+ListaSimple::~ListaSimple()
+{
+}
+
+
+
--- main.cpp
+++ main.cpp
@@ -1,40 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /* 
  * File:   main.cpp
  * Author: JOSED
+ * Compilar:    cc -o [nombre] [nombre.cpp] -lncurses
+ *              ./[nombre]
  *
  * Created on 12 de febrero de 2020, 2:36
  */
-/*
-#include <iostream>
-#include <cstdlib>
-#include <curses.h>
+
+#include <ncurses.h>
 #include "Menu.h"
 
-using namespace std;
-
-
-int main(int argc, char** argv) {
-    //Menu m;
-
-   // m.menuHome();
-    return 0;
-}
-*/
-
-#include <curses.h>
- 
 int main() {
- initscr();
-       printw("Hola linux!");
-       refresh();
-       getch();
-       endwin();
-       return 0;  
+    Menu m;
+    initscr();
+    m.menuHome();
+    return 0;  
 }
  
